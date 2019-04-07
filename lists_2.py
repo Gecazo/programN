616f31e27add1adff7cbf35ee3784aa414a9ca0b
Filip <43730405+Gecazo@users.noreply.github.com>
2019-04-07 20:18:40 +0200

    Add files via upload

---
+++ lists_2.py
@@ -0,0 +1,3 @@
+scientists = ['newton', 'einstein', 'darwin', 'tesla']
+for scientist in scientists:
+	print('I want to be like ' + scientist.capitalize() + '.')
---
+++ lits_1.py
@@ -0,0 +1,6 @@
+W = []
+w = 0
+for x in range(1, 21):
+	w = 4*(x**3) - x**2 - 4*x + 1
+	W.append(w)
+print(W)
